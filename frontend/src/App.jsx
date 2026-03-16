b1b78e47922bc7ff173293c476a4102ebf1c1a0a
Shubham Yadav <shubhamgzpyadav@gmail.com>
2026-03-16 20:31:20 +0530

    Hook Layer implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router";
 import { router } from "./app.routes.jsx";
+import { AuthProvider } from "./features/auth/auth.context.jsx";
 
-
-
-const App = () => {
-  return <RouterProvider router={router} />;
-};
+function App() {
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+}
 
 export default App;
