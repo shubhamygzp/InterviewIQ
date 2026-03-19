eadeb629d40c5dad4399e46282492795a4a174be
Shubham Yadav <shubhamgzpyadav@gmail.com>
2026-03-19 16:46:39 +0530

    Gemini API integrated in the project and it works properly

---
+++ backend/src/services/ai.service.js
@@ -0,0 +1,17 @@
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GOOGLE_GENAI_API_KEY,
+});
+
+
+async function invokeGeminiAi() {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: "Hello gemini ! Explain what is Interview ?"
+    })
+
+    console.log(response.text);
+}
+
+module.exports = invokeGeminiAi;
