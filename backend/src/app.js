10251f557cd40b994dc413676a9c753af93aa8c6
Shubham Yadav <shubhamgzpyadav@gmail.com>
2026-04-16 20:08:50 +0530

    fix: Served frontend build from backend using Express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
 
 const app = express();
 
-
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
+
+// CORS (only for local dev)
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
+}
 
 
 /* require all the routes here */
@@ -24,6 +29,16 @@ const interviewRouter = require("./routes/interview.route.js");
 /* using all the routes here */
 app.use("/api/auth", authRouter);
 app.use("/api/interview", interviewRouter);
+
+
+// Serving Frontend
+const frontendPath = path.join(__dirname, "../../frontend/dist");
+app.use(express.static(frontendPath));
+
+// Universal fallback (Express v5 safe)
+app.use((req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 
 module.exports = app;
